4d85aa1e6dda6f19a286823d250cfb9257cdfb22
jg2114-cam <jg2114@cam.ac.uk>
2024-01-31 06:35:13 +0000

    Added Task 1A tests

--- test_Task1A.py
+++ test_Task1A.py
@@ -1,4 +1,7 @@
 from Task1A import run
 
-def test_():
-    pass
+def test_Task1A(capsys):
+    captured_output = capsys.readouterr(run())
+    assert "town:" in captured_output
+    assert ("Gaw Bridge","Surfleet Seas End","Surfleet Sluice") in captured_output
+    assert "http://environment.data.gov.uk/flood-monitoring/id/stations/52119" in captured_output
