c2f892f37fb76c02ed263f4ae972e136cd59f3e7
jg2114-cam <jg2114@cam.ac.uk>
2024-01-31 06:41:19 +0000

    Fix to Task 1A Test

--- test_Task1A.py
+++ test_Task1A.py
@@ -1,7 +1,8 @@
 from Task1A import run
 
 def test_Task1A(capsys):
-    captured_output = capsys.readouterr(run())
+    run()
+    captured_output = capsys.readouterr() #Captured printed output from task 1A
     assert "town:" in captured_output
     assert ("Gaw Bridge","Surfleet Seas End","Surfleet Sluice") in captured_output
     assert "http://environment.data.gov.uk/flood-monitoring/id/stations/52119" in captured_output
